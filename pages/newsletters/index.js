176dc835fce24e13cd8999324076f82dac828c8d
niloofar-palvanehcls <niloofarpalvaneh@gmail.com>
2023-11-27 21:19:08 +0330

    respons news page

--- pages/newsletters/index.js
+++ pages/newsletters/index.js
@@ -5,11 +5,11 @@ export default function Newsletters() {
         <>
             <div className="flex items-center justify-center mb-12">
                 <div className="flex items-center justify-center flex-col">
-                    <h1 className="text-center p-6 text-xl mb-12 font-bold">
+                    <h1 className="text-center p-6 text-xl mb-12 font-bold sm:text-sm sm:leading-8">
                         با عضویت در خبرنامه از آخرین مطالب ما باخبر شوید. ما اعلان تازه ترین محصولات و یا تازه ترین وبلاگ هایمان را از طریق ایمیل و پیامک برای شما ارسال میکنیم.
                     </h1>
-                    <form className="flex flex-col gap-12">
-                        <div className="flex items-center gap-8">
+                    <form className="flex flex-col gap-12 sm:w-[90%] md:w-[95%] md:items-center">
+                        <div className="flex items-center gap-8  sm:grid sm:grid-cols-1 ">
                             <section className="flex flex-col gap-1">
                                 <label className="text-gray-700" htmlFor="firstName">نام</label>
                                 <input className="border shadow p-2 rounded outline-orange-300" type="text" name="firstName" />
@@ -19,7 +19,7 @@ export default function Newsletters() {
                                 <input className="border shadow p-2 rounded outline-orange-300" type="text" name="lastName" />
                             </section>
                         </div>
-                        <div className="flex items-center gap-8">
+                        <div className="flex items-center gap-8  sm:grid sm:grid-cols-1 ">
                             <section className="flex flex-col gap-1">
                                 <label className="text-gray-700" htmlFor="mail">ایمیل</label>
                                 <input className="border shadow p-2 rounded outline-orange-300" type="email" name="mail" />
